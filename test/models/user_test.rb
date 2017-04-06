3c0252faf91bfa502fc72ff145dd0f6190c3519d
kentrino <kentrino@h-kento.jp>
2017-04-06 21:38:57 +0900

    pr test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,12 @@ class UserTest < ActiveSupport::TestCase
     user.name = name
     assert_equal(expected_name, user.upcase_name)
   end
+
+  def test_downcase_name
+    name = 'Yamada Tarou'
+    expected = 'yamada tarou'
+    user = User.new
+    user.name = name
+    assert_equal(expected, user.downcase_name)
+  end
 end
